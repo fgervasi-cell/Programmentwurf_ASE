a0a28a1c69180d76080a7da7b477ca71db8bd20e
fgervasi-cell <gervasi.felix@student.dhbw-karlsruhe.de>
2023-03-11 11:41:25 +0100

    Added ddl statements for the database.

---
+++ tasktracker-modules/tasktracker-parent/tasktracker-plugins/src/test/java/dev/fg/dhbw/ase/tasktracker/plugins/persistence/resources/ddl.sql
@@ -0,0 +1,32 @@
+CREATE TABLE `task` (
+  `id` binary(16) NOT NULL,
+  `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `due_date` date DEFAULT NULL,
+  `description` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `reminder` date DEFAULT NULL,
+  `done` tinyint NOT NULL DEFAULT '0',
+  `task_list_id` binary(16) DEFAULT NULL,
+  `task_id` binary(16) DEFAULT NULL,
+  `creation_date` date DEFAULT NULL,
+  `completion_date` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `task_id` (`task_id`),
+  KEY `task_list_id` (`task_list_id`),
+  CONSTRAINT `task_list_id` FOREIGN KEY (`task_list_id`) REFERENCES `task_list` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `task_list` (
+  `id` binary(16) NOT NULL,
+  `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` binary(16) DEFAULT NULL,
+  PRIMARY KEY (`id`,`title`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `user` (
+  `id` binary(16) NOT NULL,
+  `mail` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`,`mail`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
