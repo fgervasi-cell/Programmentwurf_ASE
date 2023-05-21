2e656e70d70117ca61f3ffd8fdee9d6f948293a2
fgervasi-cell <gervasi.felix@student.dhbw-karlsruhe.de>
2023-05-21 09:53:51 +0200

    Added docker command to run a MySQL database.

---
+++ docker/mysql.bat
@@ -0,0 +1,6 @@
+docker run --name mysql81 -p 3406:3306 ^
+-e MYSQL_ROOT_PASSWORD=1234 ^
+-e MYSQL_USER=tasktracker ^
+-e MYSQL_PASSWORD=tasktracker ^
+-e MYSQL_DATABASE=tasktracker ^
+-d mysql/mysql-server:5.7
